466ad1cb7d2ce187e6fa7e1dd67dad6909e8ba39
Lex Toumbourou <lextoumbourou@gmail.com>
2013-09-30 19:29:27 +1000

    pep8

--- addon.py
+++ addon.py
@@ -5,15 +5,17 @@ from resources.lib import udacity_api as api
 plugin = Plugin()
 
 
-
 @plugin.route('/')
 def index():
     items = [
-        {'label': 'Course Catalog', 'path': plugin.url_for('course_catalog')},
-        {'label': 'My Courses (not implemented yet)', 'path': plugin.url_for('my_courses')}
+        {'label': 'Course Catalog',
+         'path': plugin.url_for('course_catalog')},
+        {'label': 'My Courses (not implemented yet)',
+         'path': plugin.url_for('my_courses')}
     ]
 
     return items
+
 
 @plugin.route('/course_catalog/')
 def course_catalog():
@@ -21,10 +23,11 @@ def course_catalog():
     items = [{
         'label': title,
         'path': plugin.url_for('open_course', course_id=course_id),
-        'thumbnail': thumbnail 
+        'thumbnail': thumbnail
     } for title, course_id, _, thumbnail in courses]
 
     return items
+
 
 @plugin.route('/course/<course_id>')
 def open_course(course_id):
@@ -35,8 +38,9 @@ def open_course(course_id):
             'label': title,
             'path': plugin.url_for('open_lesson', lesson_key=key)
         })
-        
+
     return items
+
 
 @plugin.route('/open_lesson/<lesson_key>')
 def open_lesson(lesson_key):
@@ -54,12 +58,14 @@ def open_lesson(lesson_key):
                 'label': title,
                 'path': plugin.url_for('open_quiz', ref_id=''),
             })
-            
+
     return items
+
 
 @plugin.route('/my_courses/')
 def my_courses():
     return []
+
 
 @plugin.route('/open_quiz/')
 def open_quiz():
@@ -68,7 +74,9 @@ def open_quiz():
 
 @plugin.route('/lectures/<lec_id>')
 def play_lecture(lec_id):
-    youtube_url = 'plugin://plugin.video.youtube/?action=play_video&videoid={0}'.format(lec_id)
+    youtube_url = (
+        "plugin://plugin.video.youtube/"
+        "?action=play_video&videoid={0}").format(lec_id)
     plugin.set_resolved_url(youtube_url)
 
 if __name__ == '__main__':
