93f6d0edb31ce48006e032f0944f3d42627ede29
Lex Toumbourou <lextoumbourou@gmail.com>
2013-12-16 22:47:17 +1100

    Black bg improvments

--- resources/lib/controls.py
+++ resources/lib/controls.py
@@ -1,7 +1,10 @@
 from xbmcswift2 import xbmcgui
 import math
+import os
 import json
 import udacity as Udacity
+
+MEDIA_DIR = os.path.dirname(os.path.dirname(__file__)) + os.sep + "media" + os.sep
 
 # To do:
 # * Clean up "magic" values
@@ -17,59 +20,71 @@ WIDGET_MAPPING = {
 
 OFFSET_X_MULTIPLIER = 0.65
 OFFSET_Y_MULTIPLIER = 0.65
-WIDGET_X_OFFSET = 15
-WIDGET_Y_OFFSET = 5
-BUTTON_WIDTH = 100
-BUTTON_HEIGHT = 50
-BUTTON_TEXT_COLOUR = '0xFFFFFFFF'
 
 
-class FormQuiz(xbmcgui.Window):
+class FormQuiz(xbmcgui.WindowDialog):
+    def __init__(self):
+        xbmcgui.Window.__init__(self)
+        self.width = 1280
+        self.height = 720
+        self.bottom_height = 70
+        self.widget_x_offset = 25
+        self.widget_y_offset = 5
+        self.widget_y_multiplier_offset = 70
+        self.button_width = 100
+        self.button_height = 50
+        self.button_text_colour = '0xFFFFFFFF'
+
     def build(self, course_id, lesson_id, group_id, asset_id, data, udacity):
         self.udacity = udacity
         self.data = data
         self.widgets = []
         self.udacity.update_activity(
             course_id, lesson_id, group_id, asset_id, 'NodeVisit')
-
-        self.width = int(self.getWidth() * 0.60)
-        self.height = int(self.getHeight() * 0.60)
-        offset_x = int((self.getWidth() * OFFSET_X_MULTIPLIER - self.width) / 2)
+        bg_image_path = MEDIA_DIR + "blank.png"
+        self.addControl(xbmcgui.ControlImage(
+            0,0, self.width, 720, bg_image_path)
+        )
         if '_background_image' in data:
             url = 'http:' + data['_background_image']['serving_url']
-            x_pos = y_pos = 0
             self.addControl(xbmcgui.ControlImage(
-                x=x_pos + offset_x, y=y_pos, width=self.width,
-                height=self.height, filename=url))
+                x=0, y=0, width=self.width,
+                height=self.height - self.bottom_height, filename=url))
 
         widgets = data['widgets']
         for widget in widgets:
             model = widget['model']
             x = int(math.ceil(
-                    widget['placement']['x'] * self.width) - WIDGET_X_OFFSET) + offset_x
+                widget['placement']['x'] * self.width) - self.widget_x_offset)
             y = int(math.ceil(
-                    widget['placement']['y'] * self.height) - WIDGET_Y_OFFSET)
+                widget['placement']['y'] * 
+                (self.height - self.widget_y_multiplier_offset)) - self.widget_y_offset)
             widget_height = int(self.height * widget['placement']['height'])
             widget_width = int(self.width * widget['placement']['width'])
 
             obj = WIDGET_MAPPING[model](
                 x=x, y=y,
-                height=widget_height, width=widget_width, label='')
+                height=widget_height, width=widget_width,  label='')
             self.addControl(obj)
             self.widgets.append({
                 'obj': obj, 'data': widget})
+
         self.submit_button = xbmcgui.ControlButton(
-            x = self.width - BUTTON_WIDTH, y = self.height + 5, width = BUTTON_WIDTH,
-            height = BUTTON_HEIGHT, label='Submit', font='font13', textColor=BUTTON_TEXT_COLOUR)
-        self.addControl(self.submit_button)
+            x = 1100, y = 660, width = self.button_width,
+            height = self.button_height, shadowColor='0xFF000000',
+            label='Submit', font='font13', textColor=self.button_text_colour)
         self.cancel_button = xbmcgui.ControlButton(
-            x = self.width - 230, y = self.height + 5, width = BUTTON_WIDTH,
-            height = BUTTON_HEIGHT, label='Cancel', font='font13', textColor=BUTTON_TEXT_COLOUR)
+            x = 970, y = 660, width = self.button_width,
+            height = self.button_height, label='Cancel',
+            font='font13', textColor=self.button_text_colour)
+
+        self.addControl(self.submit_button)
         self.addControl(self.cancel_button)
 
     def onControl(self, control):
         if control == self.cancel_button:
             self.close()
+            return
         elif control == self.submit_button:
             result = self.udacity.submit_quiz(self.data['key'], self.widgets)
             dialog = xbmcgui.Dialog()
