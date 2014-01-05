783345944ae255564b49dc4df35b14e4d79df83f
Lex Toumbourou <lextoumbourou@gmail.com>
2014-01-05 21:30:28 +1100

    Moved button names into strings.xml

--- resources/lib/controls.py
+++ resources/lib/controls.py
@@ -126,11 +126,11 @@ class FormQuiz(xbmcgui.WindowDialog):
         self.submit_button = xbmcgui.ControlButton(
             x=self.button_x_pos, y=self.button_y_pos, width=self.button_width,
             height=self.button_height, shadowColor='0xFF000000',
-            label='Submit', font='font13', textColor=self.button_text_colour)
+            label=self.plugin.get_string(30013), font='font13', textColor=self.button_text_colour)
         self.back_button = xbmcgui.ControlButton(
             x=self.button_x_pos - self.button_spacing,
             y=self.button_y_pos, width=self.button_width,
-            height=self.button_height, label='Back',
+            height=self.button_height, label=self.plugin.get_string(30014),
             font='font13', textColor=self.button_text_colour)
 
         self.addControl(self.submit_button)
