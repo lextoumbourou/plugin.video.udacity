5f1297ca09d446fa77d28ee6797ee1f416a0656a
Lex Toumbourou <lextoumbourou@gmail.com>
2014-02-05 18:11:53 +1100

    Separate tests into Unit and Integration tests

--- resources/tests/unit.py
+++ resources/tests/unit.py
@@ -1,9 +1,11 @@
 import unittest
-import sys, os
+import sys
+import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 
-from resources.lib.udacity import Udacity, UdacityAuth
+from resources.lib.udacity import Udacity
 from resources.lib.utils import widgets_to_answer
+
 
 class MockWidget():
     def getContent(self):
@@ -44,11 +46,10 @@ class UnitTests(unittest.TestCase):
         }
         udacity = Udacity(None)
         results = udacity.process_lesson_contents(data, '313947755')
-        print results
         self.assertTrue(not results[0]['answer_ref'])
 
     def test_dont_fail_when_no_image_data(self):
-        data =  {
+        data = {
             'st101': {
                 'title': 'Test 1',
                 'catalog_entry': {
@@ -64,10 +65,10 @@ class UnitTests(unittest.TestCase):
         udacity = Udacity(None)
         results = udacity.process_courses(data)
         # Check that the thumbnail field in the tuple is None
-        self.assertTrue(results[0][3] == None)
+        self.assertTrue(results[0][3] is None)
 
     def test_only_include_available_courses(self):
-        data =  {
+        data = {
             'st101': {
                 'title': 'Test 1',
                 'catalog_entry': {
@@ -84,40 +85,6 @@ class UnitTests(unittest.TestCase):
         results = udacity.process_courses(data)
         # Check that the thumbnail field in the tuple is None
         self.assertTrue(len(results) == 0)
-
-
-class OfflineTest(unittest.TestCase):
-    def test_return_true_when_cookies_and_token_are_set(self):
-        auth = UdacityAuth(
-            {'xsrf_token': 'SOMEVALUE',
-             'cookies': 'ANOTHERVALUE'})
-        self.assertTrue(auth.authenticate('test', 'pass'))
-
-
-class ApiTests(unittest.TestCase):
-    def test_get_lesson_contents(self):
-        ud = Udacity(None)
-        contents = ud.get_lesson_contents('308873795')
-        self.assertTrue(type(contents) == list)
-        self.assertTrue(type(contents[0]) == dict)
-
-    def test_get_courses(self):
-        ud = Udacity(None)
-        courses = ud.get_courses(None)
-        self.assertTrue(type(courses) == list)
-
-    def test_get_courses_filters_results(self):
-        level = 'Beginner'
-        ud = Udacity(None)
-        courses = ud.get_courses(level)
-        self.assertTrue(all([c[2] == level for c in courses]))
-
-    def test_get_course_contents(self):
-        ud = Udacity(None)
-        courses = ud.get_course_contents('cs215')
-        self.assertTrue(type(courses) == list)
-        self.assertTrue(len(courses[0]) == 3)
-
 
 if __name__ == '__main__':
     unittest.main()
